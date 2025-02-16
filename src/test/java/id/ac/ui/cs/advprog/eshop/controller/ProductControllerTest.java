50cb9b26d56babc02e099fb22986bde244d68413
izzydharma <147742855+izzydharma@users.noreply.github.com.>
2025-02-16 16:10:40 +0700

    added test for controller

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
@@ -0,0 +1,92 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.service.ProductService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.Collections;
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ProductController.class)
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService productService;
+
+    @Test
+    public void testCreateProductPage() throws Exception {
+        mockMvc.perform(get("/product/create"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("createProduct"))
+                .andExpect(model().attributeExists("product"));
+    }
+
+    @Test
+    public void testCreateProductPost() throws Exception {
+        Product product = new Product();
+        product.setProductId(UUID.randomUUID().toString());
+
+        mockMvc.perform(post("/product/create")
+                .flashAttr("product", product))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("list"));
+
+        Mockito.verify(productService).create(product);
+    }
+
+    @Test
+    public void testProductListPage() throws Exception {
+        Mockito.when(productService.findAll()).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(get("/product/list"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("productList"))
+                .andExpect(model().attributeExists("products"));
+    }
+
+    @Test
+    public void testEditProductPage() throws Exception {
+        Product product = new Product();
+        product.setProductId("1");
+        Mockito.when(productService.findAll()).thenReturn(Collections.singletonList(product));
+
+        mockMvc.perform(get("/product/edit/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("editProduct"))
+                .andExpect(model().attributeExists("product"));
+    }
+
+    @Test
+    public void testEditProductPost() throws Exception {
+        Product product = new Product();
+        product.setProductId("1");
+
+        mockMvc.perform(post("/product/edit")
+                .flashAttr("product", product))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/product/list"));
+
+        Mockito.verify(productService).update(product);
+    }
+
+    @Test
+    public void testDeleteProduct() throws Exception {
+        String productId = "1";
+
+        mockMvc.perform(get("/product/delete/" + productId))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/product/list"));
+
+        Mockito.verify(productService).delete(productId);
+    }
+}
